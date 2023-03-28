6f9a551128ff35a769e348deeeff78d88db62943
Farah Alaa <farahalaa112000@gmail.com>
2023-03-28 10:13:01 +0200

    UPDATE ACC SETTINGS

--- lib/presentation/screens/account_setting.dart
+++ lib/presentation/screens/account_setting.dart
@@ -5,6 +5,11 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smarket_app/presentation/widgets/customAppBar.dart';
 
+import 'dart:convert';
+import 'dart:io';
+//import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+
 class AccountSetting extends StatefulWidget {
   const AccountSetting({super.key});
 
@@ -13,6 +18,48 @@ class AccountSetting extends StatefulWidget {
 }
 
 class _AccountSettingState extends State<AccountSetting> {
+  // File _imageFile;
+  // String _imageUrl;
+
+  // Future<void> _uploadImage() async {
+  // final picker = ImagePicker();
+  // final pickedFile = await picker.getImage(source: ImageSource.gallery);
+  // setState(() {
+  // _imageFile = File(pickedFile.path);
+  // });
+
+  // final url = 'http://your-api-url.com/upload-image';
+  // final bytes = await _imageFile.readAsBytes();
+  // final base64Image = base64Encode(bytes);
+  // final response = await http.post(
+  // Uri.parse(url),
+  // body: {
+  // 'image': base64Image,
+  // },
+  // );
+
+  // if (response.statusCode == 200) {
+  // final jsonResponse = jsonDecode(response.body);
+  // final newImageUrl = jsonResponse['image_url'];
+  // setState(() {
+  // _imageUrl = newImageUrl;
+  // });
+
+  // Update user's profile picture in database
+  // final profileUpdateUrl = 'http://your-api-url.com/update-profile';
+  // final profileUpdateResponse = await http.post(
+  // Uri.parse(profileUpdateUrl),
+  // body: {
+  // 'user_id': '123', // replace with actual user ID
+  // 'profile_picture_url': newImageUrl,
+  // },
+  // );
+  // if (profileUpdateResponse.statusCode == 200) {
+  // print('Profile picture updated successfully');
+  // }
+  // }
+  // }
+
   var emailController = TextEditingController();
   var passwordController = TextEditingController();
   bool password = true;
@@ -82,69 +129,93 @@ class _AccountSettingState extends State<AccountSetting> {
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width - 48,
-                height: 48,
-                child: IgnorePointer(
-                  ignoring: isEditing,
-                  child: TextFormField(
-                    enabled: !isEditing,
-                    style: const TextStyle(
-                        fontFamily: "harabaraBold",
-                        color: Color(0xff333333),
-                        fontSize: 18),
-                    decoration: InputDecoration(
-                      labelText: userName,
-                      labelStyle: const TextStyle(
-                          fontFamily: "harabaraBold",
-                          color: Color(0xff2C6976),
-                          fontSize: 18),
-                      prefixIcon: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          const SizedBox(
-                            width: 24,
+            Stack(
+              alignment: AlignmentDirectional.centerEnd,
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width - 48,
+                    height: 48,
+                    child: IgnorePointer(
+                      ignoring: isEditing,
+                      child: TextFormField(
+                        enabled: !isEditing,
+                        style: const TextStyle(
+                            fontFamily: "harabaraBold",
+                            color: Color(0xff333333),
+                            fontSize: 18),
+                        decoration: InputDecoration(
+                          labelText: userName,
+                          labelStyle: const TextStyle(
+                              fontFamily: "harabaraBold",
+                              color: Color(0xff2C6976),
+                              fontSize: 18),
+                          prefixIcon: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              const SizedBox(
+                                width: 24,
+                              ),
+                              SvgPicture.asset("assets/icons/solid_user.svg"),
+                              const SizedBox(
+                                width: 14,
+                              )
+                            ],
                           ),
-                          SvgPicture.asset("assets/icons/solid_user.svg"),
-                          const SizedBox(
-                            width: 14,
-                          )
-                        ],
+                          /*suffixIcon: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              const SizedBox(
+                                width: 24,
+                              ),
+                              InkWell(
+                                  onTap: () {
+                                    setState(() {
+                                      isEditing = false;
+                                    });
+                                  },
+                                  child: SvgPicture.asset("assets/icons/edit.svg")),
+                              const SizedBox(
+                                width: 26,
+                              )
+                            ],
+                          ),*/
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                  width: 1.0,
+                                  color: Color.fromARGB(255, 44, 105, 118)),
+                              borderRadius: BorderRadius.circular(50.0)),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                  width: 1.0,
+                                  color: Color.fromARGB(255, 44, 105, 118)),
+                              borderRadius: BorderRadius.circular(50.0)),
+                        ),
                       ),
-                      suffixIcon: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          const SizedBox(
-                            width: 24,
-                          ),
-                          InkWell(
-                              onTap: () {
-                                setState(() {
-                                  isEditing = false;
-                                });
-                              },
-                              child: SvgPicture.asset("assets/icons/edit.svg")),
-                          const SizedBox(
-                            width: 26,
-                          )
-                        ],
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                              width: 1.0,
-                              color: Color.fromARGB(255, 44, 105, 118)),
-                          borderRadius: BorderRadius.circular(50.0)),
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                              width: 1.0,
-                              color: Color.fromARGB(255, 44, 105, 118)),
-                          borderRadius: BorderRadius.circular(50.0)),
                     ),
                   ),
                 ),
-              ),
+              ],
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const SizedBox(
+                  width: 24,
+                ),
+                InkWell(
+                    onTap: () {
+                      setState(() {
+                        isEditing = false;
+                      });
+                    },
+                    child: SvgPicture.asset("assets/icons/edit.svg")),
+                const SizedBox(
+                  width: 26,
+                )
+              ],
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
